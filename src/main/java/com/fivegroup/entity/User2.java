2b4d9c4fd83e69ea8cd5ffe90b542b6a05f9ba69
陈昊 <502940959@>
2019-07-25 21:57:57 +0800

    项目目录结构

--- src/main/java/com/fivegroup/entity/User2.java
+++ src/main/java/com/fivegroup/entity/User2.java
@@ -8,7 +8,7 @@ import lombok.Data;
 
 @Data
 public class User2 {
-    private Integer id123123;
+    private Integer id1231213;
     private String name ;
     private String role;
     private  String password;
