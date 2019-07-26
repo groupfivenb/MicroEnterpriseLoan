d470adb916d5ca090564ffe1191591f90ae7cf36
陈昊 <502940959@>
2019-07-26 09:09:05 +0800

    第二次改

--- src/main/java/com/fivegroup/entity/User2.java
+++ src/main/java/com/fivegroup/entity/User2.java
@@ -8,7 +8,7 @@ import lombok.Data;
 
 @Data
 public class User2 {
-    private Integer id1;
+    private Integer id11;
     private String name ;
     private String role;
     private  String password;
